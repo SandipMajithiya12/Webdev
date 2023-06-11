27b3f8f6e2e2e90e9491f85abe113ba2a8936f98
SandipMajithiya12 <sandipmajithiya10@gmail.com>
2023-06-11 13:35:51 +0530

    react first app

---
+++ dojo-blog/src/navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>the dojo blog</h1>
+            <a href="/">home</a>
+            <a href="/create">New Blog</a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
