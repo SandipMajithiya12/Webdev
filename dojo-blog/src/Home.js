9952933e30f8be85018b403e2aff0b181fefd147
SandipMajithiya12 <sandipmajithiya10@gmail.com>
2023-06-14 16:41:35 +0530

    use effect

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,28 +1,30 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs ,setBlog] = useState([
-    {
-      title : 'my new website', body : 'lorem..', author : 'yash', id :1
-    },
-    {
-      title : 'my new website', body : 'lorem..', author : 'sandip', id :3
-    },
-    {
-      title : 'my new website', body : 'lorem..', author : 'harsh', id :2
-    }
-  ]);
+  const [blogs ,setBlog] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+   
+    useEffect(() =>{
+     setTimeout(()=>{
+      fetch('http://localhost:8000/blogs')
+     .then (res => {
+      return res.json();
+     })
+     .then(data =>{
+      setBlog(data);
+      setIsPending(false);
+     });
+     },1000)
+    },[]);
+ 
   return (
     <div className="home">
-     {blogs.map((blog)=>
-     (
-      <div className="blog-preview" key={blog.id}>
-
-        <h2>{blog.title}</h2>
-        <p>writen by {blog.author} </p>
-      </div>
-     )
-     )}
+      {
+        isPending && <div>Lodaing...</div>
+      }
+     {blogs && <BlogList blogs = {blogs} title = "All blogs!" />}
+     
     </div>
   );
 };
