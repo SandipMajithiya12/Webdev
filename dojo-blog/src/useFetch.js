e7ce792f24f7c68d9fbc4db4a57b12f8c292ae74
SandipMajithiya12 <sandipmajithiya10@gmail.com>
2023-06-16 15:06:32 +0530

    todo list

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -18,7 +18,7 @@ const useFetch = (url) =>{
        })
        .then(data =>{
         setData(data);
-        setIsPending(false);
+        setIsPending(false);                
         setError(null);
        })
        .catch(err => {
