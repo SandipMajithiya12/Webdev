fbe0272233cef93626a612a04603cb15da8a553e
SandipMajithiya12 <sandipmajithiya10@gmail.com>
2023-06-16 22:03:00 +0530

    react work

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const BlogList = ({blogs,title}) => {
     return ( 
         <div className="blog-list">
@@ -7,9 +9,12 @@ const BlogList = ({blogs,title}) => {
             {blogs.map((blog)=>
      (
       <div className="blog-preview" key={blog.id}>
-
+        <Link to={`/blogs/${blog.id}`}>
         <h2>{blog.title}</h2>
         <p>writen by {blog.author} </p>
+        </Link>
+
+      
         
       </div>
      )
