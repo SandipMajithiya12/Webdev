896996678daa18ff1305eb2c8deb8768a4b50a2d
SandipMajithiya12 <sandipmajithiya10@gmail.com>
2023-06-18 14:45:40 +0530

    usehistory

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,10 +1,12 @@
-import { useState} from "react";
+import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
     const [title,setTitle] = useState('');
     const [body,setBody]= useState('');
     const [author,setAutor] = useState('');
     const [isPending,setIsPending] = useState(false);
+    const history = useHistory();
 
 
     const handleSubmit = (e) =>
@@ -23,8 +25,9 @@ const Create = () => {
     {
         console.log('new blog added');
         setIsPending(false);
+        history.push('/');
     }
-    )
+    );
 }
     return (  
 <div className="create">
