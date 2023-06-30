f9b2fb0f6ec4f8a47689a22bec2ef2cf82b513fe
SandipMajithiya12 <sandipmajithiya10@gmail.com>
2023-07-01 00:08:35 +0530

    change in weather app

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,37 @@
-
+import cold from "./images/cold.jpg"
+import download from "./images/download.jpg"
 function App() {
   return (
-    <div className="app">
-        <h2>hello</h2>
+    <div className="app" style={{
+      backgroundImage : `url(${cold})`
+    }}>
+     
+      <div className="overlay">
+        <div className="container">
+          <div className="section">
+            <div className="section_inputs">
+              <input type="text" name="city" placeholder="enter the city..." />
+              <button>F</button>
+            </div>
+           
+          <div className="section section_temrature">
+            <div className="icon">
+              <h3>rajkot ,gb</h3>
+              <img src="" alt="weather icon" />
+              <h3>cloduy</h3>
+              <div className="temperature">
+                <h1>34 c</h1>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+
     </div>
+    
+
+  
   );
 }
 
