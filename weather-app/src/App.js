89c44a66da308b8f045e8ccfd58c2fdadcda7a80
SandipMajithiya12 <sandipmajithiya10@gmail.com>
2023-07-01 12:10:44 +0530

    weathr app

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,37 +1,66 @@
-import cold from "./images/cold.jpg"
-import download from "./images/download.jpg"
+import React, { useState } from 'react';
+const api = {
+  key: "4f8e795dcd6dbf7b9f5276bff095ffc1",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&appid=${api.key}&units=metric`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
-    <div className="app" style={{
-      backgroundImage : `url(${cold})`
-    }}>
-     
-      <div className="overlay">
-        <div className="container">
-          <div className="section">
-            <div className="section_inputs">
-              <input type="text" name="city" placeholder="enter the city..." />
-              <button>F</button>
-            </div>
-           
-          <div className="section section_temrature">
-            <div className="icon">
-              <h3>rajkot ,gb</h3>
-              <img src="" alt="weather icon" />
-              <h3>cloduy</h3>
-              <div className="temperature">
-                <h1>34 c</h1>
-              </div>
-            </div>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
           </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
           </div>
         </div>
-      </div>
-
+        ) : ('')}
+      </main>
     </div>
-    
-
-  
   );
 }
 
