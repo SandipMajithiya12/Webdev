4620f63f618c5796c9ab3bd2369c69490eab3934
SandipMajithiya12 <sandipmajithiya10@gmail.com>
2023-07-07 22:52:02 +0530

    some chnages

---
+++ porfolio/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section id="about">
+        
+
+        
+    </section>
+  )
+}
+
+export default About
+
---
+++ porfolio/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full max-w-7xl'>
+        <div>
+            <div>
+               
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
